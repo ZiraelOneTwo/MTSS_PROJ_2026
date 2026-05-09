2e73dd8e8719e3c9cd84e3ec75987d30efc9a237
ZiraelOneTwo <spritzalex023@gmail.com>
2026-05-09 17:41:36 +0200

    Closes #4; throws Exception instead of returning null

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -3,12 +3,24 @@
 // [ALESSANDRO] [PADURET] [BHO]
 ////////////////////////////////////////////////////////////////////
 
+
 package it.unipd.mtss;
+import java.lang.IllegalArgumentException;
 
 public class IntegerToRoman {
+        private static String[] units           = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        private static String[] tens            = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        private static String[] hundreds        = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        private static String[] thousand        = {"", "M"};
 
         public static String convert(int number){
-                // TODO
-                return null;
+                if (number <= 0 || number > 1000) {
+                       throw new IllegalArgumentException("Invalid conversion, bad input " + number);
+                }
+
+                return  thousand        [ number / 1000 ] +
+                        hundreds        [ (number / 100) % 10 ] +
+                        tens            [ (number / 10) % 10 ] +
+                        units           [ number % 10 ];
         }
 }
