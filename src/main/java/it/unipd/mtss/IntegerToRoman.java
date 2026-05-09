73b6bf90af1aaeb151f18c4992fc6959c46f7dd4
ZiraelOneTwo <spritzalex023@gmail.com>
2026-05-09 15:34:46 +0200

    IntegerToRoman Dev started, for TDD, test must be written first

---
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////////////
+// [ALEX] [SINGH] [BHO]
+// [ALESSANDRO] [PADURET] [BHO]
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+public class IntegerToRoman {
+
+        public static String convert(int number){
+                // TODO
+                return null;
+        }
+}
